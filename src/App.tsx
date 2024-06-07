1a2996df51c83fca96034fcc6d81cb8b0b6a3966
unknown <Romangergisevich@gmail.com>
2024-06-07 23:48:00 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ThemeSwitch from './components/ui/ThemeSwitch';
+
+const App = () => {
+  return (
+    <>
+      ready to blow
+      <ThemeSwitch />
+    </>
+  );
+};
+
+export default App;
