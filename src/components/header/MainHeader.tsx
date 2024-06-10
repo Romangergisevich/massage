d8e65e9d217a79b3f70ed8f6485fb511befcd61f
unknown <Romangergisevich@gmail.com>
2024-06-10 12:33:37 +0300

    mobile menu toggle

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 import HeadLink from '../ui/buttons/HeadLink';
 import Socials from '../ui/social/Socials';
 import ThemeSwitch from '../ui/switchers/ThemeSwitch';
@@ -6,6 +6,16 @@ import styles from './MainHeader.module.css';
 import Burger from '../ui/burger/Burger';
 
 const MainHeader: React.FC = () => {
+  const mobileBar = useRef<HTMLDivElement>(null);
+  const active = styles.mobileNavBar__active;
+
+  const mobileMenuToggle = (e: ChangeEvent<HTMLInputElement>) => {
+    e.target.checked
+      ? mobileBar.current?.classList.add(active)
+      : mobileBar.current?.classList.remove(active);
+  };
+
+  console.log(mobileBar.current);
   return (
     <div className={`header ${styles.mainHeader}`}>
       <Socials />
@@ -18,7 +28,14 @@ const MainHeader: React.FC = () => {
           <HeadLink to="/">Часто задаваемые вопросы</HeadLink>
         </div>
         <div className={styles.mainHeader__mobileNavBar__container}>
-          <Burger />
+          <Burger menuToggleFunc={mobileMenuToggle} />
+        </div>
+        <div ref={mobileBar} className={styles.mainHeader__mobileNavBar__linksContainer}>
+          <HeadLink to="/">Главная</HeadLink>
+          <HeadLink to="/">Запись</HeadLink>
+          <HeadLink to="/">Противопоказания</HeadLink>
+          <HeadLink to="/">Требования к клиенту</HeadLink>
+          <HeadLink to="/">Часто задаваемые вопросы</HeadLink>
         </div>
       </div>
       <div className={styles.mainHeader__themeSwitch}>
--- src/components/ui/burger/Burger.tsx
+++ src/components/ui/burger/Burger.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 import styles from './Burger.module.css';
 
-const Burger: React.FC = () => {
+interface BurgerProps {
+  menuToggleFunc: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Burger: React.FC<BurgerProps> = (props) => {
   return (
     <div className={`${styles.hamburger} hamburger`}>
-      <input className={`${styles.checkbox} checkbox`} type="checkbox" />
+      <input
+        onChange={props.menuToggleFunc}
+        className={`${styles.checkbox} checkbox`}
+        type="checkbox"
+      />
       <svg fill="none" viewBox="0 0 50 50" height="50" width="50">
         <path
           className={`${styles.lineTop} burger_line`}
