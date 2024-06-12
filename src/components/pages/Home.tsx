1ac72a95217cbc5b7f8ceba507cfb683e22d29bd
unknown <Romangergisevich@gmail.com>
2024-06-12 19:17:37 +0300

    home page images in

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import styles from './Home.module.css';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      Расслабьтесь и избавьтесь от стресса. Откройте для себя пользу регулярных массажей
-      для вашего здоровья.
+    <div className={styles.home__container}>
+      <div>
+        <img className={styles.home__main__img} src="\src\assets\main.jpg" alt="#" />
+        <p className={styles.home__tagline}>
+          <br /> Откройте для себя пользу регулярных массажей для вашего здоровья.
+        </p>
+      </div>
+      <div className={styles.home__img__container}>
+        <div>
+          <p className={styles.second__img__tagline}>
+            Расслабьтесь и избавьтесь от стресса.
+          </p>
+          <img
+            className={styles.home__second__img}
+            src="\src\assets\massage10.jpg"
+            alt="#"
+          />
+        </div>
+        <div>
+          <img
+            className={styles.home__third__img}
+            src="\src\assets\massage11.jpg"
+            alt="#"
+          />
+        </div>
+      </div>
     </div>
   );
 };
